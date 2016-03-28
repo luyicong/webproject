ff47e35d7776e52dcf33d3935b625e56f5641386
lyc <980469887@qq.com>
2016-03-28 17:42:39 +0800

    美团html静态首页开发【3.28】

---
+++ meituan/js/ie.js
@@ -0,0 +1,3 @@
+alert('您的ie浏览器版本过低，请升级浏览器版本在进行浏览！谢谢！我们为您转到浏览器升级页面！');
+window.opener=null;window.open('','_self');window.close();
+window.location = 'http://windows.microsoft.com/zh-cn/internet-explorer/download-ie';
